641c34064ee9981d8b72b44fb14770fa9efece2e
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-02 17:59:30 +0000

    성공

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 def main():
 	a = list(map(int, input()))
-	for elem in a:
-		if elem == a[0]:
-			prevElem = a[0]
+	for idx, val in enumerate(a):
+		if idx == 0:
+			prevElem = val
 			continue
-		print("elem:",elem)
-		if prevElem<=1 or elem<=1:
-			prevElem += elem
-		else : prevElem *= elem
+		print("elem:",val)
+		if prevElem<=1 or val<=1:
+			prevElem += val
+		else : prevElem *= val
 		print("now:",prevElem)		
 	return prevElem
 
 print(main())
-#실패. if elem==a[0] 체크로직에서, 그냥 숫자가 같은 숫자면 그 숫자로 돌아가버림;
